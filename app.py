892c77aefb4ff2e0a14f72aec1413d2b5434989d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-04-05 09:45:56 -0400

    Heroku config

--- app.py
+++ app.py
@@ -56,4 +56,9 @@ async def route_chromosome(req):
   return response.text(output, headers=HEADERS, content_type='application/json')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
+  app.run(
+      host='0.0.0.0', 
+      port=int(os.environ.get('PORT', 8000)),
+      workers=int(os.environ.get('WEB_CONCURRENCY', 1)),
+      debug=bool(os.environ.get('DEBUG', ''))
+  )
