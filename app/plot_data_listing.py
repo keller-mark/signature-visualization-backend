8b3cbe4eeb0bf18edee47cd49c98fcabd19b761b
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2018-08-31 19:40:14 -0400

    Additions to ProjectData class

--- app/plot_data_listing.py
+++ app/plot_data_listing.py
@@ -3,13 +3,13 @@ from yaml import load
 from yaml import Loader
 from web_constants import *
 from signatures import Signatures
-from project_data import ProjectData
+from project_data import ProjectData, get_all_project_data_as_json
 
 
 def get_signatures_per_cancer_type():
     if not os.path.isfile(SIGS_PER_CANCER_TYPE_FILE):
       return None
-    with open(SIGS_PER_CANCER_TYPE_FILE) as data_file:    
+    with open(SIGS_PER_CANCER_TYPE_FILE) as data_file:  
       active_sigs = load(data_file, Loader=Loader)
     return active_sigs
 
@@ -18,7 +18,7 @@ def plot_data_listing():
     # instantiate a signatures object for each signature type
     signature_types = [Signatures(sig_type) for sig_type in SIG_TYPES.values()]
     return {
-      "sources": [],
+      "projects": get_all_project_data_as_json(),
       "sigs": dict([(st.get_type(), st.get_metadata()) for st in signature_types]),
-      "sig_presets": get_signatures_per_cancer_type()
+      "sig_per_cancer_type": get_signatures_per_cancer_type()
     }
--- app/project_data.py
+++ app/project_data.py
@@ -1,27 +1,55 @@
 import pandas as pd
 from web_constants import *
 
+# Helper function
 def path_or_none(proj_row, index):
     if pd.notnull(proj_row[index]):
         return proj_row[index]
     return None
 
+# Easy loading of TSV files with pandas
 def pd_fetch_tsv(s3_key, **kwargs):
     filepath = os.path.join(OBJ_DIR, s3_key)
     return pd.read_csv(filepath, sep='\t', **kwargs)
 
+""" Load the metadata file to be able to create ProjectData objects """
 meta_df = pd.read_csv(DATA_META_FILE, sep='\t', index_col=0)
+# Factory-type function for getting single ProjectData object
 def get_project_data(proj_id):
-    return ProjectData(meta_df.loc[proj_id])
+    return ProjectData(proj_id, meta_df.loc[proj_id])
 
+# Factory-type function for getting list of all ProjectData objects
 def get_all_project_data():
-    rows = meta_df.to_json(orient='records')
-    return list(map(lambda row: ProjectData(row), rows))
+    row_tuples = meta_df.to_dict(orient='index').items()
+    return list(map(lambda row: ProjectData(row[0], row[1]), row_tuples))
 
+# Factory-type function for getting 'serialized' list of all ProjectData objects
+def get_all_project_data_as_json():
+    def project_data_to_json(obj):
+        # Even though this says as_json it is really a list of python objects
+        return {
+            "id": obj.get_proj_id(),
+            "name": obj.get_proj_name(),
+            "num_donors": obj.get_proj_num_donors(),
+            "source": obj.get_proj_source(),
+            "has_clinical": obj.has_clinical_df(),
+            "has_extended": obj.has_all_extended_dfs(),
+            "has_counts": obj.has_all_counts_dfs()
+        }
+    return list(map(lambda obj: project_data_to_json(obj), get_all_project_data()))
+
+
+""" 
+Class representing a single row of the DATA_META_FILE, 
+also how the files referenced within the meta file should be loaded into data frames
+"""
 class ProjectData():
     
-    def __init__(self, proj_row):
-        self.proj_id = proj_row['id']
+    def __init__(self, proj_id, proj_row):
+        self.proj_id = proj_id
+        self.proj_name = proj_row['name']
+        self.proj_num_donors = proj_row['num_donors']
+        self.proj_source = proj_row['source']
         self.extended_paths = {}
         self.counts_paths = {}
 
@@ -36,9 +64,20 @@ class ProjectData():
             # Check for a counts file for the mutation type
             self.counts_paths[mut_type] = path_or_none(proj_row, 'path_counts_' + mut_type)
     
+    # Basic getters
     def get_proj_id(self):
         return self.proj_id
     
+    def get_proj_name(self):
+        return self.proj_name
+    
+    def get_proj_num_donors(self):
+        return self.proj_num_donors
+    
+    def get_proj_source(self):
+        return self.proj_source
+    
+    # Samples file
     def has_samples_df(self):
         return (self.samples_path != None)
 
@@ -47,6 +86,7 @@ class ProjectData():
             return pd_fetch_tsv(self.samples_path, index_col=1)
         return None
     
+    # Clinical file
     def has_clinical_df(self):
         return (self.clinical_path != None)
     
@@ -60,8 +100,15 @@ class ProjectData():
             return clinical_df
         return None
     
+    # Counts files
     def has_counts_df(self, mut_type):
         return (self.counts_paths[mut_type] != None)
+    
+    def has_all_counts_dfs(self):
+        has_all = True
+        for mut_type in SIG_TYPES.keys():
+            has_all = has_all and self.has_counts_df(mut_type)
+        return has_all
     
     def get_counts_df(self, mut_type):
         if self.has_counts_df(mut_type):
@@ -70,8 +117,15 @@ class ProjectData():
             return counts_df
         return None
     
+    # Extended mutation table files
     def has_extended_df(self, mut_type):
         return (self.extended_paths[mut_type] != None)
+    
+    def has_all_extended_dfs(self):
+        has_all = True
+        for mut_type in SIG_TYPES.keys():
+            has_all = has_all and self.has_extended_df(mut_type)
+        return has_all
     
     def get_extended_df(self, mut_type):
         if self.has_extended_df(mut_type):
